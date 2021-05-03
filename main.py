7bee370981f5db9bf2e931432dd4dfd7917409e2
Zaid Syed <59472583+zedsyed@users.noreply.github.com>
2021-05-03 14:51:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ city = input("Which city do you live in?: ")
 
 hobby = input("What is your hobby?: ")
 
-output = "Your name is {2}. You live in {0} and your hobby is {1}".format(name, city, hobby)
+output = "Your name is {}. You live in {} and your hobby is {}".format(name, city, hobby)
 
 print(output)
 
